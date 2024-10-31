a1924bd65a0680d94364a9bbbcf2560d38875d2d
Flam64 <celio.mozes.rocha@gmail.com>
2024-10-31 16:13:48 +0100

    Feat: finish challenge JS-React and Express

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export default function App() {
 	const [employee, setEmployee] = useState(sampleEmployee);
 
 	const getEmployee = () => {
-		fetch("https://randomuser.me/api?nat=en")
+		fetch("http://localhost:3310/api/employees")
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
